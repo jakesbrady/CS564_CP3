64c18c0c3402f24f04d5ea7bb4eb030a8c29a658
jswanbor <jswanbor@epic.com>
2025-07-29 17:34:21 -0500

    Just comment updates

--- BTreeNode.java
+++ BTreeNode.java
@@ -1,34 +1,41 @@
 class BTreeNode {
 
     /**
-     * Array of the keys stored in the node.
+     * Array of the keys stored in the node. Maps to studentID
      */
     long[] keys;
+
     /**
-     * Array of the values[recordID] stored in the node. This will only be filled when the node is a leaf node.
+     * Array of the values[recordID] stored in the node. This will only be filled when the node is a leaf node. Maps to recordID
      */
     long[] values;
+
     /**
      * Minimum degree (defines the range for number of keys)
      **/
     int t;
+
     /**
      * Pointers to the children, if this node is not a leaf.  If
      * this node is a leaf, then null.
      */
     BTreeNode[] children;
+
     /**
      * number of key-value pairs in the B-tree
      */
     int n;
+
     /**
      * true when node is leaf. Otherwise false
      */
     boolean leaf;
+
     /**
      * point to other previous node when it is a leaf node. Otherwise null
      */
     BTreeNode previous;
+    
     /**
      * point to other next node when it is a leaf node. Otherwise null
      */
